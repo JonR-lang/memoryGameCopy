47cfb1963ac2745f9552f923d08a7d326b17ed26
Johnny <110586185+JonR-lang@users.noreply.github.com>
2024-02-23 06:54:24 +0100

    Update Screens.jsx
    Removed the custom backdrop cause the buffering started again.

--- src/Screens.jsx
+++ src/Screens.jsx
@@ -178,7 +178,7 @@ export function PlayScreen({ end }) {
           </span>
         </span>
 
-        <div className='w-full max-w-md aspect-square bg-blue-100 rounded-xl p-3 grid grid-cols-4 place-items-center gap-3 dark:bg-black/40 dark:custom-backdrop'>
+        <div className='w-full max-w-md aspect-square bg-blue-100 rounded-xl p-3 grid grid-cols-4 place-items-center gap-3 dark:bg-black/40'>
           {getTiles(selectedValue).map((tile, i) => (
             <Tile key={i} flip={() => flip(i)} {...tile} />
           ))}
